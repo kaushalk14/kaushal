c690073c507be60b6e8969de43ed8fa16f857cee
kaushalk14 <kaushalk3216@gmail.com>
2026-08-06 12:52:34 +0530

    program program

---
+++ java program.java
@@ -0,0 +1,27 @@
+
+public class Progrm {
+    public static void main(String[] args) {
+
+        // Various data types
+        byte b = 100;
+        short s = 20000;
+        int i = 500000;
+        long l = 10000000000L;
+        float f = 12.34f;
+        double d = 123.456789;
+        char c = 'A';
+        boolean bool = true;
+        String str = "Hello, Java!";
+
+        // Print the values
+        System.out.println("Byte value: " + b);
+        System.out.println("Short value: " + s);
+        System.out.println("Int value: " + i);
+        System.out.println("Long value: " + l);
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+        System.out.println("Char value: " + c);
+        System.out.println("Boolean value: " + bool);
+        System.out.println("String value: " + str);
+    }
+}
